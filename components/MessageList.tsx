2238ae3096db3322e8957ef32f9fcb3064573426
Semokamo <43573877+Semokamo@users.noreply.github.com>
2025-06-11 12:51:23 -0400

    Update MessageList.tsx

--- components/MessageList.tsx
+++ components/MessageList.tsx
@@ -11,7 +11,7 @@ interface MessageListProps {
 
 const MessageList: React.FC<MessageListProps> = ({ messages, messagesEndRef }) => {
   return (
-    <div className="px-4 pt-4 pb-24 space-y-4 bg-gray-800/50"> {/* Removed flex-grow and overflow-y-auto custom-scrollbar */}
+    <div className="px-4 pt-4 pb-24 space-y-4 bg-gray-800/50"> {/* Removed flex-grow */}
       {messages.map((msg) => (
         <ChatBubble key={msg.id} message={msg} />
       ))}
